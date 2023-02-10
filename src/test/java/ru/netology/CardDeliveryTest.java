b157bb71dd0905d0e68381ec6ecfcb3810d51898
Alexander Maslov <AlexM-96@yandex.ru>
2023-02-10 20:47:54 +0300

    Test added

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,4 +1,25 @@
 package ru.netology;
 
+import org.junit.jupiter.api.Test;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+
 public class CardDeliveryTest {
+
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Уфа");
+        $("[data-test-id='date'] input").setValue("22.02.2023");
+        $("[data-test-id='name'] input").setValue("Иванов Иван");
+        $("[data-test-id='phone'] input").setValue("+79099654321");
+        $("[data-test-id='agreement']").click();
+        $x("//span[text()='Забронировать']").click();
+        $("[data-test-id=notification]").should(appear, Duration.ofSeconds(15));
+    }
 }
